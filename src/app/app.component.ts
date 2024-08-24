4bfc5b32d7b269071e3eee517420714dc47cac18
Mitesh0807 <miteshsavaliya7@gmail.com>
2024-08-24 18:37:18 +0530

    in progress structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: ` <router-outlet></router-outlet> `,
-  styles: [
-    `
-      :host {
-        max-width: 1280px;
-        margin: 0 auto;
-        padding: 2rem;
-        text-align: center;
-      }
-    `,
-  ],
+  template: `
+    <header class="border-b">
+      <div
+        class="container mx-auto px-4 py-4 flex justify-between items-center"
+      >
+        <div className="navbar bg-neutral text-neutral-content">
+          <a className="btn btn-ghost normal-case text-xl font-bold">MITESH </a>
+        </div>
+        <div class="flex items-center space-x-4">
+          <Button variant="ghost" size="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-rss"
+              width="60"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <circle cx="5" cy="19" r="1" />
+              <path d="M4 4a16 16 0 0 1 16 16" />
+              <path d="M4 11a9 9 0 0 1 9 9" />
+            </svg>
+            <span class="sr-only">RSS Feed</span>
+          </Button>
+          <label class="swap swap-rotate">
+            <input
+              (click)="toggleDarkMode()"
+              type="checkbox"
+              [checked]="isDarkMode()"
+            />
+
+            <svg
+              class="swap-on h-10 w-10 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"
+              />
+            </svg>
+
+            <svg
+              class="swap-off h-10 w-10 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"
+              />
+            </svg>
+          </label>
+        </div>
+      </div>
+    </header>
+    <router-outlet></router-outlet>
+  `,
+  styles: [],
 })
-export class AppComponent {}
+export class AppComponent {
+  isDarkMode = signal(false); // Define a signal to track dark mode state
+
+  toggleDarkMode() {
+    const html = document.querySelector('html');
+    const currentMode = this.isDarkMode();
+
+    if (currentMode) {
+      html?.classList.remove('dark');
+      html?.setAttribute('data-theme', 'light');
+      this.isDarkMode.set(false); // Update the signal to light mode
+    } else {
+      html?.classList.add('dark');
+      html?.setAttribute('data-theme', 'coffee');
+      this.isDarkMode.set(true); // Update the signal to dark mode
+    }
+  }
+}
--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -3,19 +3,207 @@ import { Component, signal } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  template: ` <button class="btn">Hello World</button> `,
-  styles: [
-    `
-      .logo {
-        will-change: filter;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .read-the-docs {
-        color: #888;
-      }
-    `,
-  ],
+  template: `
+    <div class="flex flex-col min-h-screen ">
+      <nav class="flex items-center flex-wrap p-5">
+        <a
+          href="/"
+          class="font-bold mr-5 text-2xl inline-flex items-center uppercase"
+          >Mitesh</a
+        >
+        <div class="ml-auto">
+          <div class="relative inline-block">
+            <a
+              href="/blog/rss.xml"
+              target="_blank"
+              class="h-12 w-12 p-1 inline-flex items-center justify-center rounded-full focus:outline-none hover:text-gray-700 dark:hover:text-white focus:bg-gray-100 dark:focus:bg-gray-800"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-label="RSS Icon"
+                fill="currentColor"
+                stroke="none"
+                width="28"
+                height="28"
+                class="inline-block"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M3 3C12.9411 3 21 11.0589 21 21H18C18 12.7157 11.2843 6 3 6V3ZM3 10C9.07513 10 14 14.9249 14 21H11C11 16.5817 7.41828 13 3 13V10ZM3 17C5.20914 17 7 18.7909 7 21H3V17Z"
+                ></path>
+              </svg>
+            </a>
+          </div>
+          <div class="relative inline-block">
+            <button
+              id="button-dark-mode"
+              class="h-12 w-12 p-1 inline-flex items-center justify-center rounded-full focus:outline-none hover:text-gray-700 dark:hover:text-white focus:bg-gray-100 dark:focus:bg-gray-800"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-label="Sun Icon"
+                fill="currentColor"
+                stroke="none"
+                width="30"
+                height="30"
+                class="inline-block"
+                viewBox="0 0 20 20"
+                id="light-mode-icon"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-label="Moon Icon"
+                fill="currentColor"
+                stroke="none"
+                width="30"
+                height="30"
+                class="inline-block"
+                viewBox="0 0 20 20"
+                id="dark-mode-icon"
+              >
+                <path
+                  d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </nav>
+
+      <div class="flex flex-col flex-grow">
+        <div class="p-5">
+          <div class="max-w-3xl mx-auto text-center mt-10 py-10">
+            <h1
+              class="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold leading-snug md:leading-snug lg:leading-snug xl:leading-snug"
+            >
+              Mitesh's Blog
+            </h1>
+            <div class="md:text-lg py-10 prose dark:prose-dark">
+              <p>
+                Hello! I am Mitesh and I love to code. JavaScript is my
+                favourite flavour. I blog about coding, software and my
+                projects.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="m-5 flex-grow max-w-screen-lg lg:mx-auto lg:flex lg:flex-wrap"
+        >
+          <div class="lg:pr-20 lg:w-2/3">
+            <!-- Repeat this block for each blog post -->
+            <div class="my-20">
+              <a
+                href="/blog/mongodb-duplicate-key-error"
+                class="group focus:outline-none"
+              >
+                <h2
+                  class="font-bold text-2xl lg:text-4xl lg:leading-snug group-hover:underline group-focus:underline"
+                >
+                  Catching MongoDB E11000 duplicate key error in Node.js
+                </h2>
+                <p class="my-5 prose dark:prose-dark">
+                  Unique indexes are useful in MongoDB for preventing duplicate
+                  data. Here's how I catch the duplicate key error so I can send
+                  a human-readable response to let users know what the problem
+                  is.
+                </p>
+              </a>
+              <p class="prose prose-sm dark:prose-dark">
+                Written
+                <span>by <a href="/blog/author/mitesh">Mitesh</a></span>
+                <span>in <a href="/blog/node">Node.js</a></span> on August 16th,
+                2024
+              </p>
+            </div>
+            <!-- Repeat this block for each blog post -->
+          </div>
+
+          <div class="w-full my-5 lg:order-last lg:mt-auto">
+            <div
+              class="p-5 text-center font-medium text-gray-900 text-opacity-60 dark:text-gray-100 dark:text-opacity-60 w-full"
+            >
+              <div class="inline-block p-3 text-gray-300 dark:text-gray-700">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-label="Previous"
+                  fill="currentColor"
+                  stroke="none"
+                  width="18"
+                  height="18"
+                  class="inline-block -mt-0.5"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <div
+                class="inline-block p-3 text-gray-900 dark:text-gray-100 underline"
+              >
+                1
+              </div>
+              <a href="/blog/page/2" class="inline-block p-3">2</a>
+              <a href="/blog/page/3" class="inline-block p-3">3</a>
+              <a href="/blog/page/4" class="inline-block p-3">4</a>
+              <a href="/blog/page/5" class="inline-block p-3">5</a>
+              <a href="/blog/page/2" class="inline-block p-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-label="Next"
+                  fill="currentColor"
+                  stroke="none"
+                  width="18"
+                  height="18"
+                  class="inline-block -mt-0.5"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+
+          <div class="lg:w-1/3 lg:p-5">
+            <div class="my-16">
+              <h3 class="uppercase text-sm font-bold py-2 text-gray-500">
+                <a href="/blog">Categories</a>
+              </h3>
+              <ul class="text-xl font-medium leading-loose">
+                <li><a href="/blog/code">Code</a></li>
+                <li><a href="/blog/deploy">Deploy</a></li>
+                <li><a href="/blog/javascript">JavaScript</a></li>
+                <li><a href="/blog/node">Node.js</a></li>
+                <li><a href="/blog/projects">Projects</a></li>
+                <li><a href="/blog/react">ReactJS</a></li>
+                <li><a href="/blog/software">Software</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <footer class="px-5 py-10 mt-auto text-sm">
+        <div class="text-center">
+          <a href="/" class="font-semibold">Mitesh</a> © 2024
+        </div>
+      </footer>
+    </div>
+  `,
+  styles: [],
 })
 export default class HomeComponent {}
