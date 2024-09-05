9776c2368ad8e3af436c36c839435dfbdd156aba
Mitesh0807 <miteshsavaliya7@gmail.com>
2024-09-05 11:32:03 +0530

    added blink terminal like cursor

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{html,ts,md}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'blink-caret': {
+          '0%, 100%': { 'border-color': 'transparent' },
+          '50%': { 'border-color': 'orange' },
+        },
+        typing: {
+          from: { width: '0' },
+          to: { width: '100%' },
+        },
+      },
+      animation: {
+        'blink-caret': 'blink-caret 0.75s step-end infinite',
+        typing: 'typing 3.5s steps(40, end) 1',
+      },
+    },
   },
   plugins: [require('daisyui')],
   daisyui: {
